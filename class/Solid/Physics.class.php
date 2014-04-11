071d7c72fbeea47ad4b4d3f785da9116253d6085
Shiso <theo.deniset@student.42.fr>
2014-04-11 18:54:01 +0200

    classes update

--- class/Solid/Physics.class.php
+++ class/Solid/Physics.class.php
@@ -2,7 +2,7 @@
 class Physics{
 	Private $_x;
 	Private $_y;
-	private $_rot = 0;
+	private $_rot = 1;
 	private $_life;
 }
 ?>
--- class/Solid/Ship.class.php
+++ class/Solid/Ship.class.php
@@ -1,5 +1,6 @@
 <?php
 include 'Ship/Motor.class.php';
+include 'Ship/Shield.class.php';
 include 'Ship/Weapon.class.php';
 abstract class Ship extends Solid{
 	private Weapon array	$_weapons[];
