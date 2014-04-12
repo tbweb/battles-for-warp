bde284cad3be9cbf4bf9bb40ee40ffb8dd3c6112
plombix <plombix@gmail.com>
2014-04-12 17:14:09 +0200

    Update Obstacle.class.php

--- class/Solid/Obstacle.class.php
+++ class/Solid/Obstacle.class.php
@@ -1,5 +1,22 @@
 <?php
-class Obstacle extends Solid{
-	
+class Obstacle extends Solid
+{
+}
+funtion stockObstacle()
+{
+	$sxy[0]=rand(5, 15);
+	$sxy[1]=rand(10, 140);
+	$sxy[2]=rand(10, 90);
+	return $sxy;
+}
+function buildObstacleSet()
+{
+		$i = 0;
+	$maxObs= rand(5, 10);
+	while(i <$maxObs)
+	{
+	$bunchObstacles[$i] = $this->stockObstacle();
+	}
+	return $bunchObstacles;
 }
 ?>
