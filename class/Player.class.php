383e7c64b4600737811ffa0bd4788202395de696
Shiso <theo.deniset@student.42.fr>
2014-04-12 21:06:38 +0200

    none

--- class/Player.class.php
+++ class/Player.class.php
@@ -19,37 +19,45 @@ class Player {
 		$this->_pwdCrypt = hash("whirlpool", $pwdCrypt);
 		$this->_race = $race;
 	}
+
 	public function getName() {
 		return $this->_name;
 	}
+
 	public function setName($name) {
 		$this->_name = $name;
 		return $this;
 	}
+
 	public function getRace() {
 		return $this->_race;
 	}
+
 	public function setRace($race) {
 		$this->_race = $race;
 		return $this;
 	}
+
 	public function getIdPlayer() {
 		return $this->_id_player;
 	}
+
 	public function getIdGame() {
 		return $this->_id_game;
 	}
+
 	public function setIdGame($id_game) {
 		$this->_id_game = $id_game;
 		return $this;
 	}
+
 	public function getPwdcrypt() {
 		return $this->_pwdCrypt;
 	}
+
 	public function setPwdcrypt($pwdCrypt) {
 		$this->_pwdCrypt = $pwdCrypt;
 		return $this;
 	}
-	
 }
 ?>
