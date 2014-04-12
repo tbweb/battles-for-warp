839903f45fd92fba1ae314eb2930be4971d0bcf4
Shiso <theo.deniset@student.42.fr>
2014-04-12 17:47:42 +0200

    Physics Now manage size of objects

--- class/Solid/Physics.class.php
+++ class/Solid/Physics.class.php
@@ -1,7 +1,9 @@
 <?php
 class Physics{
-	private $_pos = new Position();
-	private $_life;
+	private		$_pos = new Position();
+	private int	$_w;
+	private int	$_h;
+	private		$_life;
 
 	public function set_pos($p)	{ $this->_pos = $p; }
 	public function get_pos()	{ return $this->_pos; }
