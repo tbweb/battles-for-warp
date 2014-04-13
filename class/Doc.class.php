c5743feacee0001569a335e2b9ca2a0e2cb19a87
Paul RENVOISE <prenvois@e1r9p20.42.fr>
2014-04-13 18:21:26 +0200

    [Add] trait Doc, Playship.doc.txt; [Fix] changed some docs, Added 'use Doc' prerogative

--- class/Doc.class.php
+++ class/Doc.class.php
@@ -4,10 +4,12 @@ trait Doc
 {
 	public static function doc()
 	{
-		$filename = get_class($this).".class.txt";
+		$filename = get_class($this).".doc.txt";
 		$doc = "<- ".get_class($this)." ----------------------------------------------------------------------". PHP_EOL;
 		if (file_exists($filename))
-			$doc .= file_get_contents($filename);
+			$doc .= file_get_contents($filename)."\n";
+		else
+			$doc .= "No documentation for this class yet.\n";
 		$doc .= "---------------------------------------------------------------------- ".get_class($this)." ->". PHP_EOL;
 		return $doc;
 	}
--- class/Game/PlayShip.class.php
+++ class/Game/PlayShip.class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once '../Doc.class.php';
+
 class PlayShip
 {
 	private				$_id = 1;
@@ -15,6 +17,9 @@ class PlayShip
 	private				$_shield;
 	private				$_inertia;
 	private				$_played;
+	public static		$verbose = FALSE;
+
+	use Doc;
 
 	public function __construct($name, $x, $y)
 	{
@@ -23,103 +28,122 @@ class PlayShip
 		$this->_pc = $this->_ship->getPc();
 	}
 
-	public static function doc() {
-		$file = "No documentation for this class.";
-		if (file_exists("./Ship.doc.txt"))
-			$file = file_get_contents("./Ship.doc.txt");
-		return ($file);
-	}
-	
 	public function getId() {
 		return $this->_id;
 	}
+
 	public function setId($_id) {
 		$this->_id = $_id;
 		return $this;
 	}
+
 	public function getIdGame() {
 		return $this->_id_game;
 	}
+
 	public function setIdGame($_id_game) {
 		$this->_id_game = $_id_game;
 		return $this;
 	}
+
 	public function getIdPlayer() {
 		return $this->_id_player;
 	}
+
 	public function setIdPlayer($_id_player) {
 		$this->_id_player = $_id_player;
 		return $this;
 	}
+
 	public function getIdShip() {
 		return $this->_id_ship;
 	}
+
 	public function setIdShip($_id_ship) {
 		$this->_id_ship = $_id_ship;
 		return $this;
 	}
-	public function getShipName() {
-		return $this->_ship_name;
-	}
-	public function setShipName($_ship_name) {
-		$this->_ship_name = $_ship_name;
-		return $this;
-	}
-	public function getWay() {
-		return $this->_way;
-	}
-	public function setWay($_way) {
-		$this->_way = $_way;
-		return $this;
-	}
-	public function getX() {
-		return $this->_x;
-	}
-	public function setX($_x) {
-		$this->_x = $_x;
-		return $this;
-	}
-	public function getY() {
-		return $this->_y;
-	}
-	public function setY($_y) {
-		$this->_y = $_y;
-		return $this;
-	}
-	public function getPc() {
-		return $this->_pc;
-	}
-	public function setPc($_pc) {
-		$this->_pc = $_pc;
-		return $this;
-	}
-	public function getShield() {
-		return $this->_shield;
-	}
-	public function setShield($_shield) {
-		$this->_shield = $_shield;
-		return $this;
-	}
-	public function getInertia() {
-		return $this->_inertia;
-	}
-	public function setInertia($_inertia) {
-		$this->_inertia = $_inertia;
-		return $this;
-	}
-	public function getPlayed() {
-		return $this->_played;
-	}
-	public function setPlayed($_played) {
-		$this->_played = $_played;
-		return $this;
-	}
+
 	public function getShip() {
 		return $this->_ship;
 	}
+
 	public function setShip($_ship) {
 		$this->_ship = $_ship;
 		return $this;
 	}
+
+	public function getShipName() {
+		return $this->_ship_name;
+	}
+
+	public function setShipName($_ship_name) {
+		$this->_ship_name = $_ship_name;
+		return $this;
+	}
+
+	public function getWay() {
+		return $this->_way;
+	}
+
+	public function setWay($_way) {
+		$this->_way = $_way;
+		return $this;
+	}
+
+	public function getX() {
+		return $this->_x;
+	}
+
+	public function setX($_x) {
+		$this->_x = $_x;
+		return $this;
+	}
+
+	public function getY() {
+		return $this->_y;
+	}
+
+	public function setY($_y) {
+		$this->_y = $_y;
+		return $this;
+	}
+
+	public function getPc() {
+		return $this->_pc;
+	}
+
+	public function setPc($_pc) {
+		$this->_pc = $_pc;
+		return $this;
+	}
+
+	public function getShield() {
+		return $this->_shield;
+	}
+
+	public function setShield($_shield) {
+		$this->_shield = $_shield;
+		return $this;
+	}
+
+	public function getInertia() {
+		return $this->_inertia;
+	}
+
+	public function setInertia($_inertia) {
+		$this->_inertia = $_inertia;
+		return $this;
+	}
+
+	public function getPlayed() {
+		return $this->_played;
+	}
+
+	public function setPlayed($_played) {
+		$this->_played = $_played;
+		return $this;
+	}
+
 }
 ?>
